ec30a0ea261ed8dd4cecfcb68ccc935999a3e14d
Zai Santillan <zaisantillan14@gmail.com>
2023-05-11 22:48:24 +0800

    init: supa-rps

---
+++ supa-rps/ContentView.swift
@@ -0,0 +1,19 @@
+//  Date: 5/9/23
+//
+//  Author: Zai Santillan
+//  Github: @plskz
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ supa-rps/supa_rpsApp.swift
@@ -0,0 +1,16 @@
+//  Date: 5/9/23
+//
+//  Author: Zai Santillan
+//  Github: @plskz
+
+
+import SwiftUI
+
+@main
+struct supa_rpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
