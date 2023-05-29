4623ad3ac7a625571737c67a424e8a5c09b873f1
Zai Santillan <zaisantillan14@gmail.com>
2023-05-29 10:10:45 +0800

    feat: fetch current session on app launch

--- supa-rps/ContentView.swift
+++ supa-rps/ContentView.swift
@@ -11,13 +11,30 @@ struct ContentView: View {
     @State private var isLoggedIn: Bool = false;
     
     var body: some View {
-        if isLoggedIn {
-            MainView(isLoggedIn: $isLoggedIn)
-        } else {
-            NavigationView {
-                LoginView(isLoggedIn: $isLoggedIn)
-                    .padding()
+        Group {
+            if isLoggedIn {
+                MainView(isLoggedIn: $isLoggedIn)
+            } else {
+                NavigationView {
+                    LoginView(isLoggedIn: $isLoggedIn)
+                        .padding()
+                }
             }
+        }
+        .task {
+            await getCurrentSession()
+        }
+    }
+    
+    private func getCurrentSession() async {
+        do {
+            let session = try await client.auth.session
+            if !session.accessToken.isEmpty {
+                // Session exists, user is logged in
+                isLoggedIn = true
+            }
+        } catch {
+            print("Error fetching session: \(error)")
         }
     }
 }
